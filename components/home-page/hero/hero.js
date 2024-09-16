b9a2f83fdfb82736234b3d3aa0de1f531d0de794
Richard Essuman <ressuman001@gmail.com>
2024-09-16 09:44:00 +0000

    "Added Hero component with image, heading, and paragraph elements"

--- components/home-page/hero/hero.js
+++ components/home-page/hero/hero.js
@@ -1,3 +1,25 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
+
 export default function Hero() {
-  return <div></div>;
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/hero-image.avif"
+          width={300}
+          height={300}
+          alt="An image showing Richard"
+        />
+      </div>
+      <h1>Hi, I&apos;m Richard- A Web Developer and Tech Enthusiast</h1>
+      <p>
+        I am currently honing my skills in modern web development technologies,
+        focusing on Next.js. I&apos;ve been working with Pages Router,
+        Server-Side Rendering (SSR), and building dynamic web applications. My
+        aim is to develop fast, SEO-friendly, and scalable solutions using these
+        advanced frameworks.
+      </p>
+    </section>
+  );
 }
