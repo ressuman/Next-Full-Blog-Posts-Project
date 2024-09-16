7e9b775935830029a24f7cf51f16f9ea147f08b1
Richard Essuman <ressuman001@gmail.com>
2024-09-16 15:14:38 +0000

    "Updated PostItem component to include image path and link to post details"

--- components/posts/post-item/post-item.js
+++ components/posts/post-item/post-item.js
@@ -2,13 +2,19 @@ import Link from "next/link";
 import classes from "./post-item.module.css";
 import Image from "next/image";
 import { formattedDate } from "@/utils/date";
+import { imagePath } from "@/utils/image";
 
 export default function PostItem({ title, image, excerpt, date, slug }) {
   return (
     <li className={classes.post}>
-      <Link>
+      <Link href={`/posts/${slug}`}>
         <div className={classes.image}>
-          <Image src="" width="" height="" alt="" />
+          <Image
+            src={imagePath(slug, image)}
+            width={300}
+            height={200}
+            alt={title}
+          />
         </div>
 
         <div className={classes.content}>
---
+++ utils/image.js
@@ -0,0 +1,1 @@
+export const imagePath = (slug, image) => `/images/posts/${slug}/${image}`;
