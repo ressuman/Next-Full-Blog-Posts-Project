5f400592ada2329327301ea91b1803d41911a318
Richard Essuman <ressuman001@gmail.com>
2024-09-17 20:14:22 +0000

    "Added getFeaturedPosts function to filter posts by isFeatured property"

---
+++ helpers/featured-posts.js
@@ -0,0 +1,9 @@
+import { getAllPosts } from "./all-posts";
+
+export function getFeaturedPosts() {
+  const allPosts = getAllPosts();
+
+  const featuredPosts = allPosts.filter((post) => post.isFeatured);
+
+  return featuredPosts;
+}
