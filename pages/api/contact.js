19780eccd98cbb6ebef8f2a8183736a8bc038dd8
Richard Essuman <ressuman001@gmail.com>
2024-09-19 11:49:43 +0000

    Refactor API routes: Remove hello.js and add contact.js
    This commit removes the hello.js file from the API routes and adds a new contact.js file. The contact.js file handles POST requests and validates the input before storing the message. This refactor improves the organization and functionality of the API routes.

---
+++ pages/api/contact.js
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      email.trim() === "" ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid input" });
+
+      return;
+    }
+
+    const newMessage = {
+      email,
+      name,
+      message,
+      date: new Date(),
+    };
+
+    res.status(201).json({
+      message: "Successfully stored message!.",
+      message: newMessage,
+    });
+  }
+}
