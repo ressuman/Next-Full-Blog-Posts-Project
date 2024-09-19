8c7b92a2755f2b71b4339d0103d77d5f0ed413d0
Richard Essuman <ressuman001@gmail.com>
2024-09-19 11:32:21 +0000

    "Added ContactForm import and replaced empty div with ContactForm component in ContactPage"

---
+++ components/contact/contact-form/contact-form.js
@@ -0,0 +1,31 @@
+import classes from "./contact-form.module.css";
+
+export default function ContactForm() {
+  return (
+    <section className={classes.contact}>
+      <h1>How can I help you?</h1>
+
+      <form className={classes.form}>
+        <div className={classes.controls}>
+          <div className={classes.control}>
+            <label htmlFor="email">Your Email</label>
+            <input type="email" name="email" id="email" required />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="name">Your FullName</label>
+            <input type="text" name="name" id="name" required />
+          </div>
+        </div>
+
+        <div className={classes.control}>
+          <label htmlFor="message">Your Message</label>
+          <textarea name="message" id="message" rows="5" required></textarea>
+        </div>
+
+        <div className={classes.actions}>
+          <button type="submit">Send Message</button>
+        </div>
+      </form>
+    </section>
+  );
+}
